1346ba6740c6a47ed0d55db0b33282554b64ad0e
Mayank Nader <mayank.nader11@gmail.com>
2022-03-30 05:30:16 +0530

    build(config): Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+import ghs
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name="ghs",
+    version=ghs.__version__,
+    description="Get you Github profile's stats and summary.",
+    packages=["ghs"],
+    include_package_data=True,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="http://github.com/interviewstreet/ghs",
+    license="MIT",
+    author="Hackerrank",
+    author_email="pypi@hackerrank.com",
+    keywords=['github', 'cli', 'utility', 'command', 'console'],
+    install_requires=["requests", "termcolor", "colorama", "pyperclip", "halo"],
+    entry_points={"console_scripts": ["ghs=ghs.ghs:main"]},
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ]
+)
