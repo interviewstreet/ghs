bc0f5ab747b9b8de51623e15069ae98cb943285a
HarshCasper <erbeusgriffincasper@gmail.com>
2022-04-04 13:39:34 +0530

    chore: format code using black

--- ghs/check_config.py
+++ ghs/check_config.py
@@ -13,89 +13,97 @@ from ghs.es_queries import viewer_query
 colorama.init()
 home_dir = os.path.expanduser("~")
 config = configparser.ConfigParser()
-spinner = Halo(text='Checking if the token is valid', spinner='dots')
+spinner = Halo(text="Checking if the token is valid", spinner="dots")
 my_session = retry(Session(), retries=2, backoff_factor=10)
 
 
 def config_dir_path():
-  return os.path.join(home_dir, ".ghs")
+    return os.path.join(home_dir, ".ghs")
 
 
 def config_file_path():
-  return os.path.join(config_dir_path(), "ghs.config")
+    return os.path.join(config_dir_path(), "ghs.config")
 
 
 class ValidationException(Exception):
-  pass
+    pass
 
 
 def check_config_dir(spnr):
-  try:
-    path = config_dir_path()
-    # makes path recursively. returns None if already exist.
-    os.makedirs(path, exist_ok=True)
-    if not os.path.isfile(os.path.join(path, "ghs.config")):
-      spnr.stop()
-      return create_config_file()
-  except IOError:
-    print("Error occured while creating config files.")
+    try:
+        path = config_dir_path()
+        # makes path recursively. returns None if already exist.
+        os.makedirs(path, exist_ok=True)
+        if not os.path.isfile(os.path.join(path, "ghs.config")):
+            spnr.stop()
+            return create_config_file()
+    except IOError:
+        print("Error occured while creating config files.")
 
-  return True
+    return True
 
 
 def create_config_file():
-  cprint(
-      "Creating config file",
-      color="green",
-  )
+    cprint(
+        "Creating config file",
+        color="green",
+    )
 
-  return save_token()
+    return save_token()
 
 
 # TODO: move this to fetchers after resolving circular imports
 def fetch_token_scopes(headers):
-  resp = requests.get(
-      f'https://api.github.com/rate_limit', headers=headers)
-  
-  if 'X-OAuth-Scopes' in resp.headers.keys():
-    scopes = resp.headers['X-OAuth-Scopes']
-    return scopes.split(', ')
-  else:
-    return None
+    resp = requests.get(f"https://api.github.com/rate_limit", headers=headers)
+
+    if "X-OAuth-Scopes" in resp.headers.keys():
+        scopes = resp.headers["X-OAuth-Scopes"]
+        return scopes.split(", ")
+    else:
+        return None
 
 
 def validate_token_scopes(headers):
-  required_scopes = ['read:user', 'repo', 'read:packages']
-  token_scopes = fetch_token_scopes(headers)
-  if token_scopes is None or not set(required_scopes).issubset(token_scopes):
-    raise ValidationException(f"Error: The token does not have valid scopes. \n Required scopes: {required_scopes}. \n Provided token scopes: {token_scopes} ")
+    required_scopes = ["read:user", "repo", "read:packages"]
+    token_scopes = fetch_token_scopes(headers)
+    if token_scopes is None or not set(required_scopes).issubset(token_scopes):
+        raise ValidationException(
+            f"Error: The token does not have valid scopes. \n Required scopes: {required_scopes}. \n Provided token scopes: {token_scopes} "
+        )
+
 
 def save_token():
-  pat = input("please enter your github pat: ")
+    pat = input("please enter your github pat: ")
 
-  headers = {
-      "Authorization": f"token {pat}", 'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)'}
+    headers = {
+        "Authorization": f"token {pat}",
+        "User-Agent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)",
+    }
 
-  spinner.start()
-  with my_session.post('https://api.github.com/graphql', json={'query': viewer_query()}, headers=headers) as result:
-    request = result
-  spinner.stop()
+    spinner.start()
+    with my_session.post(
+        "https://api.github.com/graphql",
+        json={"query": viewer_query()},
+        headers=headers,
+    ) as result:
+        request = result
+    spinner.stop()
 
-  if request.status_code == 200:
-    result = request.json()
-    username = result['data']['viewer']['login']
-    validate_token_scopes(headers)
-    print(f"Saving the token for {username} in ~/.ghs/ghs.config")
-    config["TOKEN"] = {"pat": pat}
-    with open(config_file_path(), "w") as f:
-      config.write(f)
-    return True
-  elif request.status_code == 401:
-    raise ValidationException("The PAT is not valid")
-  else:
-    raise ValidationException("Error in saving the pat")
+    if request.status_code == 200:
+        result = request.json()
+        username = result["data"]["viewer"]["login"]
+        validate_token_scopes(headers)
+        print(f"Saving the token for {username} in ~/.ghs/ghs.config")
+        config["TOKEN"] = {"pat": pat}
+        with open(config_file_path(), "w") as f:
+            config.write(f)
+        return True
+    elif request.status_code == 401:
+        raise ValidationException("The PAT is not valid")
+    else:
+        raise ValidationException("Error in saving the pat")
 
 
 def get_saved_token():
-  config.read(config_file_path())
-  return config["TOKEN"]["pat"]
+    config.read(config_file_path())
+    return config["TOKEN"]["pat"]
--- ghs/utils.py
+++ ghs/utils.py
@@ -3,85 +3,93 @@ from dateutil.relativedelta import relativedelta
 from ghs.check_config import get_saved_token
 
 USER_AGENTS = [
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko)',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko)',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko)',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko)',
-    'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0',
-    'Apache/2.4.34 (Ubuntu) OpenSSL/1.1.1 (internal dummy connection)',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36',
-    'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.4.0) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.1 TV Safari/538.1',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/78.0.3904.70 Safari/537.36',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
-    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko; Google Web Preview) Chrome/41.0.2272.118 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
-    'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.42 (KHTML, like Gecko) Chromium/25.0.1349.2 Chrome/25.0.1349.2 Safari/537.42',
-    'Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
-    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
-    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)'
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko)",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0",
+    "Apache/2.4.34 (Ubuntu) OpenSSL/1.1.1 (internal dummy connection)",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36",
+    "Mozilla/5.0 (SMART-TV; Linux; Tizen 2.4.0) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.1 TV Safari/538.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/78.0.3904.70 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko; Google Web Preview) Chrome/41.0.2272.118 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0",
+    "Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.42 (KHTML, like Gecko) Chromium/25.0.1349.2 Chrome/25.0.1349.2 Safari/537.42",
+    "Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)",
 ]
 
 
 def get_random_user_agent():
-  return USER_AGENTS[random.randint(0, len(USER_AGENTS)-1)]
+    return USER_AGENTS[random.randint(0, len(USER_AGENTS) - 1)]
 
 
 def get_headers():
-  headers = {"Authorization": f"token {get_saved_token()}",
-             'User-Agent': get_random_user_agent()}
-  return headers
+    headers = {
+        "Authorization": f"token {get_saved_token()}",
+        "User-Agent": get_random_user_agent(),
+    }
+    return headers
 
 
 def subtract_years(date, years):
-  return format_date_object((date - relativedelta(years=years)))
+    return format_date_object((date - relativedelta(years=years)))
 
 
 def format_date_object(date):
-  return date.strftime("%Y-%m-%dT%H:%M:%SZ")
+    return date.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 def let_user_pick(options):
-  print("Please choose:")
-  for idx, element in enumerate(options):
-    print("{}) {}".format(idx+1, element))
-  i = input("Enter number: ")
-  try:
-    if 0 < int(i) <= len(options):
-      return int(i)
-  except:
-    pass
-  return None
+    print("Please choose:")
+    for idx, element in enumerate(options):
+        print("{}) {}".format(idx + 1, element))
+    i = input("Enter number: ")
+    try:
+        if 0 < int(i) <= len(options):
+            return int(i)
+    except:
+        pass
+    return None
 
 
 def parse_user_contribution_repos(repos, user_contribution_repos):
-  for repo in repos[:5]:
-    commits_count = repo['contributions']['totalCount']
-    repo_name = repo['repository']['name']
-    repo_owner = repo['repository']['owner']['login']
-    languages = repo['repository']['languages']['nodes']
-    is_private = repo['repository']['isPrivate']
-    stargazer_count = repo['repository']['stargazerCount']
-    fork_count = repo['repository']['forkCount']
+    for repo in repos[:5]:
+        commits_count = repo["contributions"]["totalCount"]
+        repo_name = repo["repository"]["name"]
+        repo_owner = repo["repository"]["owner"]["login"]
+        languages = repo["repository"]["languages"]["nodes"]
+        is_private = repo["repository"]["isPrivate"]
+        stargazer_count = repo["repository"]["stargazerCount"]
+        fork_count = repo["repository"]["forkCount"]
 
-    if repo_name in user_contribution_repos.keys():
-      user_contribution_repos[repo_name]['commits_count'] += commits_count
-    else:
-      user_contribution_repos[repo_name] = {'owner': repo_owner, 'commits_count': commits_count,
-                                            'languages': languages, 'is_private': is_private, 'stargazer_count': stargazer_count, 'fork_count': fork_count}
+        if repo_name in user_contribution_repos.keys():
+            user_contribution_repos[repo_name]["commits_count"] += commits_count
+        else:
+            user_contribution_repos[repo_name] = {
+                "owner": repo_owner,
+                "commits_count": commits_count,
+                "languages": languages,
+                "is_private": is_private,
+                "stargazer_count": stargazer_count,
+                "fork_count": fork_count,
+            }
 
-  return user_contribution_repos
+    return user_contribution_repos
